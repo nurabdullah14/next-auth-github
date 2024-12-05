5cfc7a47d20a04dad6248d00cacb52d745b28646
nurabdullah14 <nur.abdullah@aditya.co.id>
2024-12-05 23:11:18 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/master/' : '',
+  basePath: isProd ? '/master' : '',
+  output: 'export'
+};
 
 export default nextConfig;
